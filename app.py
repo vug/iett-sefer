a372f5b2f939d4969bf02ad442708c672c5a13b0
Ugur Guney <ugurguney@gmail.com>
2020-07-02 16:50:32 -0400

    Plot multilines connecting last N positions of a bus.

--- app.py
+++ app.py
@@ -1,26 +1,31 @@
 """
 bokeh serve --show app.py
 """
-import time
+from collections import defaultdict
 
-from bokeh.models import ColumnDataSource
+from bokeh.models import ColumnDataSource, MultiLine
 from bokeh.plotting import figure, curdoc
 from bokeh.tile_providers import CARTODBPOSITRON, get_provider
 import numpy as np
 
-from snapshot import Snapshot, load_latest_snapshot
+from snapshot import load_latest_snapshots
 
 
-def wgs84_to_web_mercator(xs, ys):
-    """Convert lat, lon to Web Mercator coords.
+def lat2wmlat(lat: float) -> float:
+    """Convert latitude in degrees to Web Mercator latitude.
 
     Taken from https://stackoverflow.com/questions/57178783/how-to-plot-latitude-and-longitude-in-bokeh
     """
     K = 6378137
-    wm_lons = [x * (K * np.pi / 180.0) for x in xs]
-    wm_lats = [np.log(np.tan((90 + y) * np.pi / 360.0)) * K for y in ys]
+    wm_lat = np.log(np.tan((90 + lat) * np.pi / 360.0)) * K
+    return wm_lat
 
-    return (wm_lons, wm_lats)
+
+def lon2wmlon(lon: float) -> float:
+    """Convert longidute in degrees to Web Mercator longitude."""
+    K = 6378137
+    wm_lon = lon * (K * np.pi / 180.0)
+    return wm_lon
 
 
 p = figure(
@@ -37,44 +42,55 @@ p = figure(
 tile_provider = get_provider(CARTODBPOSITRON)
 p.add_tile(tile_provider)
 
-source = ColumnDataSource({"lat": [], "lon": [], "licence": []})
+# lines connecting successive N locations of a bus
+line_source = ColumnDataSource({"lat": [], "lon": [], "licence": []})
+glyph = MultiLine(xs="lon", ys="lat", line_color="blue", line_width=1, line_alpha=0.5)
+bus_line_renderer = p.add_glyph(line_source, glyph)
 
-bus_renderer = p.circle(
+# disks indicating latest location of a bus
+circle_source = ColumnDataSource({"lat": [], "lon": [], "licence": []})
+bus_circle_renderer = p.circle(
     x="lon",
     y="lat",
-    size=5,
-    # fill_color="col",
+    size=2.5,
     fill_color="blue",
     line_color=None,
     fill_alpha=0.5,
-    source=source,
+    source=circle_source,
 )
-
-ds = bus_renderer.data_source
-
-
-def update1():
-    import random
-
-    new_data = {}
-    xs, ys = wgs84_to_web_mercator(
-        [28.8 + random.random() * 0.4], [40.9 + random.random() * 0.3]
-    )
-    new_data["lon"] = ds.data["lon"] + xs
-    new_data["lat"] = ds.data["lat"] + ys
-    new_data["licence"] = ds.data["licence"] + ["abidin"]
-    ds.data = new_data
-    print("Hi!")
 
 
 def update():
-    ss: Snapshot = load_latest_snapshot()
-    vehicle_data = [(v.Boylam, v.Enlem, v.Plaka, v.Saat) for v in ss.vehicles]
-    xs, ys, licences, times = zip(*vehicle_data)
-    wm_lons, wm_lats = wgs84_to_web_mercator(xs=xs, ys=ys)
-    new_data = {"lon": wm_lons, "lat": wm_lats, "license": licences, "time": times}
-    ds.data = new_data
-    print("Hi!", ss.ts, time.time(), len(ss.vehicles), xs[:3], ys[:3], licences[:3])
+    snapshots = load_latest_snapshots(2)
+
+    lic2bus = defaultdict(list)
+    # Group each snapshot by license plates
+    # license -> [buses with that license in successive snapshots]
+    for snapshot in snapshots:
+        for bus in snapshot.vehicles:
+            if bus.Plaka is None:
+                continue
+            lic2bus[bus.Plaka].append(bus)
+
+    new_data = {
+        "lon": [[lon2wmlon(bus.Boylam) for bus in buses] for buses in lic2bus.values()],
+        "lat": [[lat2wmlat(bus.Enlem) for bus in buses] for buses in lic2bus.values()],
+        "license": list(lic2bus.keys()),
+        "time": [buses[-1].Saat for buses in lic2bus.values()],
+    }
+
+    bus_line_renderer.data_source.data = new_data
+    print(
+        "Hi!",
+        new_data["lon"][:3],
+        new_data["lat"][:3],
+        new_data["license"][:3],
+        new_data["time"][:3],
+    )
+
+    new_data["lon"] = [vals[-1] for vals in new_data["lon"]]
+    new_data["lat"] = [vals[-1] for vals in new_data["lat"]]
+    bus_circle_renderer.data_source.data = new_data
 
 
 update()
